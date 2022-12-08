6554cbe8dd45d3a9d69605c5b4ffaf361fd33a51
Saba <saba.sadegh67@gmail.com>
2022-12-08 17:15:00 +0330

    content when the page loads

--- app.js
+++ app.js
@@ -80,3 +80,23 @@ const menu = [
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
 ];
+
+const sectionCenter = document.querySelector(".section-center");
+window.addEventListener("DOMContentLoaded", function () {
+  let displayMenu = menu.map(function (item) {
+    return `<article class="menu-item">
+    <img src=${item.img} alt="menu item" class="photo" />
+    <div class=${item.title}>
+      <header>
+        <h4>${item.title}</h4>
+        <h4 class="price">$${item.price}</h4>
+      </header>
+      <p class="item-text">
+     ${item.desc}
+      </p>
+    </div>
+  </article>`;
+  });
+  displayMenu = displayMenu.join("");
+  sectionCenter.innerHTML = displayMenu;
+});
